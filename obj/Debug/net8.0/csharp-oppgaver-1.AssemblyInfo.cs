6b5c15acf867ad5ede86d696ca759acccfda89aa
LHT082024 <LHT082024@gmail.com>
2024-09-04 10:24:31 +0200

    tried string method

--- obj/Debug/net8.0/csharp-oppgaver-1.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharp-oppgaver-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-oppgaver-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d322eaa65a9cf081e2d76933c3624ddf66ef807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0dd67eb33dc8a729fc7be31dff243f489b864c")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-oppgaver-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-oppgaver-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
